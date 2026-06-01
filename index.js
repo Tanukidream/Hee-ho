ae87167360953c68e7daa03e3b97d5d03642b55d
Tanukidream <dario.penelas.soler@gmail.com>
2026-06-01 13:10:08 +0200

    Update index.js
    fix 1.0.0.2

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hee-Ho bot online ❄️');
+});
+
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🌐 Listening on ${PORT}`);
+});
+
 const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const fs = require('fs');
 
